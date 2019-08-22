b847736b4a77645e823110645de7ea63a69e2e05
Cris Daniluk <cdaniluk@rhythmictech.com>
2019-08-22 14:41:02 -0400

    more fixes

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_ec2_transit_gateway" "tgw" {
 resource "aws_ec2_transit_gateway_vpc_attachment" "tgw_attach" {
   subnet_ids         = var.subnet_ids
   transit_gateway_id = aws_ec2_transit_gateway.tgw.id
-  vpc_id             = module.vpc.vpc_id
+  vpc_id             = var.vpc_id
 }
 
 resource "aws_ram_resource_share" "tgw_share" {
