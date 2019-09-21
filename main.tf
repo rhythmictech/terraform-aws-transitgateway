c5d7a9636e880b1dc5f8552aa14dccf11527152b
Cris Daniluk <cdaniluk@rhythmictech.com>
2019-09-21 15:43:19 -0400

    Don't try to create resource shares when not explicitly needed to avoid errors on accounts that don't have RAM enabled

--- main.tf
+++ main.tf
@@ -11,6 +11,7 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "tgw_attach" {
 }
 
 resource "aws_ram_resource_share" "tgw_share" {
+  count                     = length(var.resource_share_accounts) > 0 ? 1 : 0
   name                      = "${var.name}-tgw-share"
   allow_external_principals = false
   tags                      = merge(local.base_tags, var.additional_tags)
@@ -19,10 +20,11 @@ resource "aws_ram_resource_share" "tgw_share" {
 resource "aws_ram_principal_association" "tgw_ram_principal" {
   count              = length(var.resource_share_accounts)
   principal          = var.resource_share_accounts[count.index]
-  resource_share_arn = aws_ram_resource_share.tgw_share.arn
+  resource_share_arn = aws_ram_resource_share.tgw_share[0].arn
 }
 
 resource "aws_ram_resource_association" "tgw_ram_resource" {
+  count              = length(var.resource_share_accounts) > 0 ? 1 : 0
   resource_arn       = aws_ec2_transit_gateway.tgw.arn
-  resource_share_arn = aws_ram_resource_share.tgw_share.arn
+  resource_share_arn = aws_ram_resource_share.tgw_share[0].arn
 }
