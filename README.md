bee560ae9aa4924ccb5a0fac7a221b9d4876987e
Cris Daniluk <cdaniluk@rhythmictech.com>
2020-01-09 13:54:43 -0500

    update project desc and vars

--- README.md
+++ README.md
@@ -1,17 +1,25 @@
 # terraform-aws-transitgateway
 [![](https://github.com/rhythmictech/terraform-aws-transitgateway/workflows/check/badge.svg)](https://github.com/rhythmictech/terraform-aws-transitgateway/actions)
 
-Tag the world
+Manage a Transit Gateway resource. Supports RAM.
 
 <!-- BEGINNING OF PRE-COMMIT-TERRAFORM DOCS HOOK -->
 ## Inputs
 
 | Name | Description | Type | Default | Required |
 |------|-------------|:----:|:-----:|:-----:|
+| amazon\_side\_asn | Amazon Side ASN | number | n/a | yes |
+| description | Description used in Transit Gateway | string | n/a | yes |
+| name | Name of transit gateway and related resources | string | `"vpc"` | no |
+| resource\_share\_accounts | Accounts to share gateway with \(leave blank for none\) | list(string) | `[]` | no |
+| subnet\_ids | Subnets to create transit gateway in \(must use one subnet for every AZ you wish to use the tgw in\) | list(string) | `[]` | no |
+| tags | Tags to add to any resources that support it | map(string) | `{}` | no |
+| vpc\_id | VPC to create transit gateway in | string | n/a | yes |
 
 ## Outputs
 
 | Name | Description |
 |------|-------------|
+| transit\_gateway\_id |  |
 
 <!-- END OF PRE-COMMIT-TERRAFORM DOCS HOOK -->
--- variables.tf
+++ variables.tf
@@ -1,35 +1,39 @@
-variable "description" {
-  type = string
-}
 
 variable "amazon_side_asn" {
-  type = number
+  type        = number
+  description = "Amazon Side ASN"
 }
 
-variable "subnet_ids" {
-  type    = list(string)
-  default = []
-}
-
-variable "vpc_id" {
-  type = string
-}
-
-variable "resource_share_accounts" {
-  type    = list(string)
-  default = []
-}
-
-variable "tags" {
-  default = {}
-  type    = map(string)
-}
-
-variable "region" {
-  type = string
+variable "description" {
+  type        = string
+  description = "Description used in Transit Gateway"
 }
 
 variable "name" {
-  default = "vpc"
-  type    = string
+  type        = string
+  default     = "vpc"
+  description = "Name of transit gateway and related resources"
+}
+
+variable "resource_share_accounts" {
+  type        = list(string)
+  default     = []
+  description = "Accounts to share gateway with (leave blank for none)"
+}
+
+variable "subnet_ids" {
+  type        = list(string)
+  default     = []
+  description = "Subnets to create transit gateway in (must use one subnet for every AZ you wish to use the tgw in)"
+}
+
+variable "tags" {
+  type        = map(string)
+  default     = {}
+  description = "Tags to add to any resources that support it"
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "VPC to create transit gateway in"
 }
