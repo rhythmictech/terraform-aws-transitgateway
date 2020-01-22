09c0d0512b6ecf3666d5494bb67d353a98a8a6d1
Steven B <genseb7@gmail.com>
2020-01-22 14:11:02 -0500

    add tgw to outputs.tf (#2)
    * Update outputs.tf
    
    * bump versions in pre-commit
    
    * update readme

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,9 @@
 output "transit_gateway_id" {
-  value = aws_ec2_transit_gateway.tgw.id
+  description = "ID of Transit Gateway"
+  value       = aws_ec2_transit_gateway.tgw.id
+}
+
+output "transit_gateway" {
+  description = "Transit Gateway Resource"
+  value       = aws_ec2_transit_gateway.tgw
 }
